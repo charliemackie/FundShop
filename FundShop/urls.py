458481a48ed97a9db2604e20ad49ccb741d6824d
charliemackie <51030219+charliemackie@users.noreply.github.com>
2021-11-02 22:41:15 -0400

    login page

--- FundShop/urls.py
+++ FundShop/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from store.views import index, login
 
 urlpatterns = [
+    path('', login),
     path('admin/', admin.site.urls),
+    path('index/', index)
 ]
---
+++ store/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def login(request):
+
+    context = {}
+    
+    return render(request, 'store/login.html', context)
